35ff76388604f0971222c76eb5913210b67d0500
yousef ellban <ellbanyousef@gmail.com>
2026-01-25 20:27:14 +0200

    Add Gson,Glide and Retrofit Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,4 +41,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 }
