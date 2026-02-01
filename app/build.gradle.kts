eb2b02a7dcb48312c8567a8a5d30a5461fa0b785
yousef ellban <ellbanyousef@gmail.com>
2026-02-01 23:31:58 +0200

    Discovery UI design Implementation for Categories

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     implementation("androidx.navigation:navigation-fragment:2.5.3")
     implementation("androidx.navigation:navigation-ui:2.5.3")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     implementation("com.pierfrancescosoffritti.androidyoutubeplayer:core:12.1.0")
-
+    implementation("com.google.android.material:material:1.11.0")
 }
